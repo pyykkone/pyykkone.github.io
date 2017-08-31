bb6d46a9eafdb991bd40a528a223bfc6b10db277
pyykkone <tmipyykkonegmail.com>
2017-08-31 17:02:20 +0300

    update

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	
+	
+	console.log('...');
+	
+	$('#content').append('<h1>Testi..</h1>');
+
+});
---
+++ ymparistoluvat/temp/example_permitdata_orig.js
@@ -0,0 +1,10 @@
+
+
+enviromentalPermits=[
+{ "asia" : "Iso-Antin Kanala", asiaID : "12341", "type" : "Tuotantoelaimia", "coords" : [  60.9 , 24.9 ] , "luvanMyontaja" : "Ville Virkailija" ,  "luvanHakija" : "Suklaakanat Oyt"  }
+,{ "asia" : "Timon Ydinvoimala", asiaID : "12342", "type" : "Energia", "coords" : [  61.9 , 24.3 ] , "luvanMyontaja" : "Ville Virkailija" ,  "luvanHakija" : "Nuclear Nightclutb Oy"  }
+,{ "asia" : "Keijon Keinosähkö", asiaID : "12343", "type" : "Energia", "coords" : [  62.5 , 23.3 ] , "luvanMyontaja" : "Vivian Virkailija" ,  "luvanHakija" : "Eazy-E's Energy Oy"  }
+,{ "asia" : "Pasilan Lajittelu", asiaID : "12344", "type" : "Lajittelu", "coords" : [  60.19 , 24.95 ] , "luvanMyontaja" : "A. J." ,  "luvanHakija" : "Suklaakanat Oyt"  }
+,{ "asia" : "Lasi,murskaksi", asiaID : "12345", "type" : "Lajittelu", "coords" : [  65.19 , 27.95 ] , "luvanMyontaja" : "A. J." ,  "luvanHakija" : "Suklaakanat Oyt"  }
+,{ "asia" : "Chocolate Chicken", asiaID : "12346" , "type" : "Tuotantoelaimia", "coords" : [  64.57 , 9.5 ] , "luvanMyontaja" : "A. J." ,  "luvanHakija" : "Suklaakanat Oyt"  }
+]	
