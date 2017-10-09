94a0b487b73cd615f003111cff12b4d76e1f06ef
pyykkone <tmipyykkonegmail.com>
2017-10-09 11:12:30 +0300

    update

--- kartta_demo/JS/functions.js
+++ kartta_demo/JS/functions.js
@@ -4,13 +4,12 @@
 $(document).ready(function() {	
 
 
-	yearsArr = [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]//[1987,1988,1989,1990,1991,1992];
-	//yearsArr = [1997,2015]
-	// Remove
 	
-	g = $('g');
-	g.splice(0, 2)
-	g.filter(function(x) { $(this).attr('postinumero').substring(0,1)=='0' })
+	var yearsArr = kuntadata.map(function(obj) { return obj.vuosi; });
+	yearsArr = yearsArr.filter(function(v,i) { return yearsArr.indexOf(v) == i; });
+
+	//	yearsArr = [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015]//[1987,1988,1989,1990,1991,1992];
+
 	
 	for (var i=0;i<yearsArr.length;i++) {
 		
@@ -30,21 +29,17 @@ $(document).ready(function() {
 	
 	cols = ['red','blue','green','orange'];
 	
-	/// Random for coloring
-	function randomIntFromInterval(min,max)		{
-		return Math.floor(Math.random()*(max-min+1)+min);
-	}
 	
 	
 	function changeMapYear(year) {
-		console.log(year)
+		
 		g = $('g');
 		g.splice(0, 2);
 		g.map(function(x) { 
 			
-			pno = $(this).attr('postinumero');
+			pno = $(this).attr('kuntakoodi');
 			
-			tmp = postinumerodata.filter(function(x) { return(x['postinumero']==pno) })
+			tmp = kuntadata.filter(function(x) { return(x['kuntakoodi']==pno) })
 			
 			tmp = tmp.filter(function(x) { return(x['vuosi'] == year)  });
 			
@@ -56,7 +51,7 @@ $(document).ready(function() {
 					$(this).append('<div>'+tmp[0]['arvo']+'</div>');
 					
 					$(this).click(function() {
-						alert('Postinumero: '+$(this).attr('postinumero')+', arvo: ' + $(this).children('div')[0].innerHTML );
+						alert('Kunta: '+$(this).attr('kuntakoodi')+', arvo: ' + $(this).children('div')[0].innerHTML );
 					});
 				}
 			}
